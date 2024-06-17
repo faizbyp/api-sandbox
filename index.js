ad4cf8cb0fbe3f53fa6d590655a89f86e511cfc8
faizbyp <faizbyp@gmail.com>
2024-06-17 13:55:56 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+// MIDDLEWARE TO PARSE JSON AUTOMATICALLY
+app.use(express.json());
+
+app.get("/tshirt", (req, res) => {
+  res.status(200).send({
+    tshirt: "Uniqlo Airism",
+    size: "large",
+  });
+});
+
+app.post("/tshirt/:id", (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(418).send({ message: "Logo is a must!" });
+  }
+
+  res.send({
+    tshirt: `Tshirt with logo ${logo} and id: ${id}`,
+  });
+});
+
+app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
